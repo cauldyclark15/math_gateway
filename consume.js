f6423b7d59a3e40db7a64316de191020de6a2b25
joseliecast <joselie@laguro.com>
2020-03-28 10:52:33 +0800

    working websocket and rabbitmq

---
+++ consume.js
@@ -0,0 +1,69 @@
+const amqp = require('amqplib/callback_api');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const CONN_URL =
+  'amqp://szfhfngd:a5kKGzXCpV8bAdDIb3dGflSSWbxus4I0@cougar.rmq.cloudamqp.com/szfhfngd';
+amqp.connect(CONN_URL, function (err, conn) {
+  conn.createChannel(function (err, ch) {
+    ch.consume(
+      'addition',
+      async function (msg) {
+        const publishToQueue = async (queueName, data) => {
+          if (ch) {
+            ch.sendToQueue(queueName, new Buffer(data));
+          }
+        };
+
+        console.log('.....');
+        await publishToQueue('sum', msg.content.toString());
+        console.log('published sum');
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
+});
+
+// localChannel.consume(
+//   'product',
+//   function (msg) {
+//     console.log('.....');
+//     console.log('Message:', msg.content.toString());
+
+//     ws.send(msg.content.toString());
+//   },
+//   {
+//     noAck: true,
+//   }
+// );
+
+// localChannel.consume(
+//   'difference',
+//   function (msg) {
+//     console.log('.....');
+//     console.log('Message:', msg.content.toString());
+
+//     ws.send(msg.content.toString());
+//   },
+//   {
+//     noAck: true,
+//   }
+// );
+
+// localChannel.consume(
+//   'quotient',
+//   function (msg) {
+//     console.log('.....');
+//     console.log('Message:', msg.content.toString());
+
+//     ws.send(msg.content.toString());
+//   },
+//   {
+//     noAck: true,
+//   }
+// );
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const amqp = require('amqplib/callback_api');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const WebSocket = require('ws');
 
+const wss = new WebSocket.Server({ port: 8080 });
 const app = express();
 
 app.use(cors());
@@ -12,9 +14,27 @@ const CONN_URL =
   'amqp://szfhfngd:a5kKGzXCpV8bAdDIb3dGflSSWbxus4I0@cougar.rmq.cloudamqp.com/szfhfngd';
 
 let localChannel = null;
-amqp.connect(CONN_URL, function (err, conn) {
-  conn.createChannel(function (err, channel) {
-    localChannel = channel;
+
+wss.on('connection', function connection(ws) {
+  ws.send('test1');
+
+  amqp.connect(CONN_URL, function (err, conn) {
+    conn.createChannel(function (err, channel) {
+      localChannel = channel;
+
+      localChannel.consume(
+        'sum',
+        function (msg) {
+          console.log('.....');
+          console.log('Message:', msg.content.toString());
+
+          ws.send(msg.content.toString());
+        },
+        {
+          noAck: true,
+        }
+      );
+    });
   });
 });
 
@@ -31,14 +51,73 @@ process.on('exit', (code) => {
   console.log(`Closing rabbitmq channel`);
 });
 
-app.post('/message', async (req, res, next) => {
-  const { queueName, payload } = req.body;
-  console.log({ queueName, payload });
+app.post('/multiply', async (req, res, next) => {
+  const { payload } = req.body;
+  console.log({
+    payload,
+  });
 
   try {
-    await publishToQueue(queueName, payload);
+    await publishToQueue('multiplication', payload);
 
-    res.json({ message: 'sent' });
+    res.json({
+      message: 'sent',
+    });
+    next();
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+app.post('/divide', async (req, res, next) => {
+  const { payload } = req.body;
+  console.log({
+    payload,
+  });
+
+  try {
+    await publishToQueue('division', payload);
+
+    res.json({
+      message: 'sent',
+    });
+    next();
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+app.post('/add', async (req, res, next) => {
+  const { payload } = req.body;
+  console.log({
+    payload,
+  });
+
+  try {
+    await publishToQueue('addition', payload);
+
+    res.json({
+      message: 'sent',
+    });
+    next();
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+app.post('/subtract', async (req, res, next) => {
+  const { queueName, payload } = req.body;
+  console.log({
+    queueName,
+    payload,
+  });
+
+  try {
+    await publishToQueue('subtraction', payload);
+
+    res.json({
+      message: 'sent',
+    });
     next();
   } catch (error) {
     throw new Error(error);
